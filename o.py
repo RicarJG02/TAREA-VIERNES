90e345635d11fd74e1c37793c00127cb61f33a5f
RicarJG02 <60985532+RicarJG02@users.noreply.github.com>
2020-03-06 08:21:09 -0600

    Delete o.py

--- o.py
+++
@@ -1,8 +0,0 @@
-def aCadena(n,base):
-   cadenaConversion = "0123456789ABCDEF"
-   if n < base:
-      return cadenaConversion[n]
-   else:
-      return aCadena(n//base,base) + cadenaConversion[n%base]
-
-print(aCadena(2048,16))
